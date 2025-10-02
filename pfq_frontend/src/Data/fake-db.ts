18b77f9bcb32cbad33d82cb96478895b4cce5341
dylanhollohan <dylanhollohan@gmail.com>
2025-10-02 12:53:13 -0300

    adds fake quotes

---
+++ pfq_frontend/src/Data/fake-db.ts
@@ -0,0 +1,7 @@
+export const fakeDB = [
+  { id: 1, quote: "The only way to do great work is to love what you do.", author: "Steve Jobs" },
+  { id: 2, quote: "Life is what happens when you're busy making other plans.", author: "John Lennon" },
+  { id: 3, quote: "Get busy living or get busy dying.", author: "Stephen King" },
+  { id: 4, quote: "You have within you right now, everything you need to deal with whatever the world can throw at you.", author: "Brian Tracy" },
+  { id: 5, quote: "Believe you can and you're halfway there.", author: "Theodore Roosevelt" },
+]
