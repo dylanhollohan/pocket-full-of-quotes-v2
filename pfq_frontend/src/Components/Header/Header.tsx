a48b631cc0bc089fc9b27dbb53019e4664fcb09b
dylanhollohan <dylanhollohan@gmail.com>
2025-10-02 12:04:00 -0300

    move header to own folder

--- pfq_frontend/src/Components/Header/Header.tsx
+++ pfq_frontend/src/Components/Header/Header.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './Header.scss';
 
+import philosopher from '../../assets/philosopher.png';
+
 const Header: React.FC = () => {
   return (
     <header className="header">
+      <img src={philosopher} alt="Logo" className="logo" />
       <h1 className="title">Pocket Full of Quotes</h1>
+      <span className="login">Log In placeholder</span>
     </header>
   );
 };
