9f0c9c8d98d9e9e7b2b40180f9fc0089c3e0747d
dylanhollohan <dylanhollohan@gmail.com>
2025-10-02 12:03:01 -0300

    remove unused scaffold, adds footer

--- pfq_frontend/src/App.tsx
+++ pfq_frontend/src/App.tsx
@@ -1,8 +1,6 @@
-// import { useState } from 'react'
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.scss';
-import Header from './Components/Header';
+import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer';
 
 function App() {
 
@@ -10,20 +8,11 @@ function App() {
     <>
       <Header />
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
+      <Footer />
     </>
   )
 }
---
+++ pfq_frontend/src/Components/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.scss';
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="footer">
+      <h2 className="creator">Created by Dylan Hollohan</h2>
+    </footer>
+  );
+};
+
+export default Footer;
